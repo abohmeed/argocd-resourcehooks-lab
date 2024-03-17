7f47f18ab9948325a9511a43fbd3023cf149738c
Ahmed Elfakharany <abohmeed@gmail.com>
2024-03-17 15:51:10 +0100

    Demonstrates the sync hook

---
+++ sync-example/redis-app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask
+from redis import Redis, RedisError
+
+# Retrieve Redis password from environment variable
+redis_password = os.environ.get('REDIS_PASSWORD')
+
+# Connect to Redis with authentication
+redis = Redis(host='localhost', port=6379, db=0, decode_responses=True, password=redis_password)
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    try:
+        visits = redis.incr('counter')  # Increment the counter in Redis
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
+
+    html = "<h3>Hello from Flask!</h3>" \
+           "<b>Visits:</b> {visits}"
+    return html.format(visits=visits)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
+
